8d4a3f0fe6366bba916509a37add694c372ad340
aakanksha-k <136099041+aakanksha-k@users.noreply.github.com>
2024-07-02 19:46:19 +0530

    Initial commit on deployment-branch

---
+++ src/components/Dino.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
+import dinoImage from '../Assets/dinosaur.png';
+
+const DinoContainer = styled.div`
+  position: absolute;
+  bottom: 0;
+  left: 50px;
+  width: 50px;
+  height: 50px;
+  background: url(${dinoImage}) no-repeat center/contain;
+  transition: transform 0.2s;
+`;
+
+const Dino = () => {
+  const [isJumping, setIsJumping] = useState(false);
+
+  const handleKeyPress = (e) => {
+    if (e.key === ' ' || e.key === 'ArrowUp') {
+      setIsJumping(true);
+      setTimeout(() => setIsJumping(false), 500);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('keydown', handleKeyPress);
+    return () => {
+      window.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []);
+
+  return <DinoContainer style={{ transform: isJumping ? 'translateY(-150px)' : 'translateY(0)' }} />;
+};
+
+export default Dino;
+
+
